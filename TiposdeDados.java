449f3e5f3753cc4b28afe90546c3b547a90dc2aa
lz-r3at00 <115791534+lz-r3at00@users.noreply.github.com>
2023-04-19 15:55:44 -0300

    Add files via upload

---
+++ TiposdeDados.java
@@ -0,0 +1,16 @@
+
+package ProjetoRevisãoJV;
+
+public class TiposdeDados {
+    public static void main(String[] args) {
+        String LZ = "Luiz";
+        System.out.println("Exemplo de String: "+LZ);
+        
+        Double notas = 1.2;
+        System.out.println("Exemplo de Double:"+notas);
+        
+        int sinuca = 50;
+        System.out.println("Exemplo de int:"+sinuca);
+        
+    }
+}
